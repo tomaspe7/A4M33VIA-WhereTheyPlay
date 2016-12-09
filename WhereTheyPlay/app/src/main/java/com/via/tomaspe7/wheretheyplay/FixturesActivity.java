22ea7956a6146efdb381eb274edf7e7e3173d9de
tomaspe7 <tomaspe7@gmail.com>
2016-12-09 14:38:37 +0100

    initial project commit

---
+++ WhereTheyPlay/app/src/main/java/com/via/tomaspe7/wheretheyplay/FixturesActivity.java
@@ -0,0 +1,13 @@
+package com.via.tomaspe7.wheretheyplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FixturesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fixtures);
+    }
+}
