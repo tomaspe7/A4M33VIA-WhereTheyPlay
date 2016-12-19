10d046ae8a112cdc22412895adc73d6d5a6a4ebb
tomaspe7 <tomaspe7@gmail.com>
2016-12-19 17:44:31 +0100

    added server code of own API

---
+++ WhereTheyPlay_server/Server.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+
+flask = Flask(__name__)
+cors = CORS(flask)
+stadium_names = {"Deportivo Alavés": "Estadio de Mendizorroza",
+                 "Athletic Club": "Estadio San Mamés",
+                 "Club Atlético de Madrid": "Vicente Calderón",
+                 "FC Barcelona": "Camp Nou",
+                 "Real Betis": "Estadio Benito Villamarín",
+                 "RC Celta de Vigo": "Estadio de Balaídos",
+                 "RC Deportivo La Coruna": "Estadio Municipal de Riazor",
+                 "SD Eibar": "Ipurua",
+                 "RCD Espanyol": "Cornellà-El Prat",
+                 "Granada CF": "Estadio Nuevo Los Cármenes",
+                 "UD Las Palmas": "Estadio Gran Canaria",
+                 "CD Leganés": "Estadio Municipal de Butarque",
+                 "Málaga CF": "Estadio La Rosaleda",
+                 "CA Osasuna": "Estadio El Sadar",
+                 "Real Madrid CF": "Santiago Bernabéu",
+                 "Real Sociedad de Fútbol": "Estadio Anoeta",
+                 "Sevilla FC": "Estadio Ramón Sánchez-Pizjuán",
+                 "Sporting Gijón": "Estadio El Molinón",
+                 "Valencia CF": "Estadio de Mestalla",
+                 "Villarreal CF": "Estadio El Madrigal"}
+
+
+@flask.route('/stadium', methods=['GET'])
+def get_hello_world():
+    club = request.args['club']
+    return stadium_names[club]
